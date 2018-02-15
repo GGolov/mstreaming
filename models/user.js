9462f60f36eb8bfac98bc591b67dd9b861fe9bfe
GGolov <gleb.golov@gmail.com>
2018-02-15 02:27:01 +0100

    Added user model

--- models/user.js
+++ models/user.js
@@ -1,1 +1,37 @@
+const crypto = require('crypto')
 const mongoose = require('mongoose')
+const passwordHash = require('password-hash')
+
+mongoose.connect('mongodb://localhost/musicstreaming')
+
+const db = mongoose.connection
+
+const UserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    index: true
+  },
+  email: {
+    type: String,
+    index: true
+  },
+  password: {
+    type: String
+  },
+  salt: {
+    type: String
+  },
+  registrationDate: {
+    type: Date
+  }
+})
+
+const User = module.exports = mongoose.model('User', UserSchema)
+
+module.exports.createUser = (newUser, callback) => {
+  const values = passwordHash.sha512(newUser.password)
+
+  newUser.password = values.passwordHash
+  newUser.salt = values.salt
+  newUser.save(callback)
+}
--- routes/signup.js
+++ routes/signup.js
@@ -1,15 +1,18 @@
 const router = require('express').Router()
+const user = require('../models/user')
 
 router.get('/', (req, res) => {
   res.render('signup')
 })
 
 router.post('/', (req, res) => {
+  let username = req.body.username
   let email = req.body.email
   let password = req.body.password
   let confirmPassword = req.body.confirmPassword
 
   // Validation
+  req.checkBody('username', 'Username is required').notEmpty()
   req.checkBody('email', 'Email is required').notEmpty()
   req.checkBody('email', 'Email is not valid').isEmail()
   req.checkBody('password', 'Password is required').notEmpty()
@@ -23,6 +26,18 @@ router.post('/', (req, res) => {
     })
   }
   else {
+    const newUser = new User({
+      name: username,
+      email: email,
+      password: password,
+      registrationDate: new Date()
+    })
+
+    user.newUser(newUser, (err, user) => {
+      if (err) throw err
+      console.log(user)
+    })
+
     console.log(`Registration of ${email} completed successfully`)
   }
 })
